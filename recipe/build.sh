dc291e59cfa7923c33ba31fbf627b17ad6548e50
Kai Tietz <ktietz@anaconda.com>
2021-08-13 14:44:19 +0200

    add support for osx-arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Get an updated config.sub and config.guess
+cp -r ${BUILD_PREFIX}/share/libtool/build-aux/config.* .
+
 ./configure --prefix=$PREFIX --with-tiff=$PREFIX --with-jpeg=$PREFIX
 make -j${CPU_COUNT}
 make check
